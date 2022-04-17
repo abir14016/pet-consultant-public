30faa08f27df9da38f943b403e446cc26562b357
abir14016 <abir14016@gmail.com>
2022-04-17 21:15:09 +0600

    facebook,github and google sign in done!!

--- src/Pages/Login/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/Login/SocialLogin/SocialLogin.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../../../firebase.init';
 import './SocialLogin.css'
 
 const SocialLogin = () => {
     const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+
     const [signInWithGithub, githubUser, githubLoading, githubError] = useSignInWithGithub(auth);
 
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+
+
     let errorElement;
-    if (googleError || githubError) {
-        errorElement = <div><p>Error: {googleError?.message} {githubError?.message}</p></div>
+    if (googleError || githubError || facebookError) {
+        errorElement = <div><p>Error: {googleError?.message} {githubError?.message} {facebookError.message}</p></div>
     }
     let loadingElemrnt;
-    if (googleLoading || githubLoading) {
+    if (googleLoading || githubLoading || facebookLoading) {
         loadingElemrnt = <p>Loading...</p>
     }
 
     const navigate = useNavigate()
-    if (googleUser || githubUser) {
+    if (googleUser || githubUser || facebookUser) {
         navigate('/')
     }
     return (
@@ -35,7 +39,7 @@ const SocialLogin = () => {
                 <div className='d-flex justify-content-center'>
                     <button onClick={() => signInWithGoogle()} className='social-logo-btn'><img style={{ width: "50px" }} src="images/social-logo/google-logo.png" alt="" /></button>
                     <button onClick={() => signInWithGithub()} className='social-logo-btn mx-3'><img style={{ width: "50px" }} src="images/social-logo/github-logo.png" alt="" /></button>
-                    <button className='social-logo-btn'><img style={{ width: "50px" }} src="images/social-logo/facebook-logo.png" alt="" /></button>
+                    <button onClick={() => signInWithFacebook()} className='social-logo-btn'><img style={{ width: "50px" }} src="images/social-logo/facebook-logo.png" alt="" /></button>
                 </div>
             </div>
         </div>
